c9424bbf3798ea87496538a90ca72043fdc1a4d4
Sorin George Budescu <4bottiglie@gmail.com>
2018-02-26 19:59:07 +0100

    Initial Commit

---
+++ ImageUtilsKit/ImageUtilsKit.h
@@ -0,0 +1,19 @@
+//
+//  ImageUtilsKit.h
+//  ImageUtilsKit
+//
+//  Created by Sorin George Budescu on 26/02/18.
+//  Copyright © 2018 Sorin George Budescu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImageUtilsKit.
+FOUNDATION_EXPORT double ImageUtilsKitVersionNumber;
+
+//! Project version string for ImageUtilsKit.
+FOUNDATION_EXPORT const unsigned char ImageUtilsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageUtilsKit/PublicHeader.h>
+
+
